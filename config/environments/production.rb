77c7f397169addc35cfe3c1bb2f94de19594323f
javiel <zehua046@gmail.com>
2019-08-18 11:26:40 +0800

    add mailgun production

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -1,6 +1,10 @@
 Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
+  config.action_mailer.default_url_options = { :host => '139.162.69.54' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = config_for(:email).symbolize_keys
+
   # Code is not reloaded between requests.
   config.cache_classes = true
 
